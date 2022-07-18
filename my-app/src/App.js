0e600b06cef88a6487b98e105ddfeb4705217b9d
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2022-07-17 23:23:10 -0400

    adding new expense component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,6 @@
+
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 // const App = () => {...}
 function App() {
@@ -22,7 +24,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items = {expenses} />
     </div>
   );
