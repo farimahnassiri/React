503e72daf3d29a0760fa9a99b04c31ba342ba551
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2022-07-18 22:53:40 -0400

    using multi-mono states

--- my-app/src/components/NewExpense/ExpenseForm.js
+++ my-app/src/components/NewExpense/ExpenseForm.js
@@ -1,32 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    //this event is native to vanila and we can use it here too
-    const titleChangeHandler = (event) => {
-        console.log(event.target.value);
-    };
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
-return <form>
-    <div className='new-expense__controls'>
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  return (
+    <form>
+      <div className='new-expense__controls'>
         <div className='new-expense__control'>
-            <label>Title</label>
-            <input type='text' onChange={titleChangeHandler}/>
+          <label>Title</label>
+          <input type='text' onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
-            <label>Amount</label>
-            <input type='number' min='0.01' step='0.01'/>
+          <label>Amount</label>
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
-            <label>Amount</label>
-            <input type='date' min='2019-01-01' max='2022-12-31'/>
+          <label>Date</label>
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
+            onChange={dateChangeHandler}
+          />
         </div>
-    </div>
-    <div className='new-expense__actions'>
+      </div>
+      <div className='new-expense__actions'>
         <button type='submit'>Add Expense</button>
-    </div>
-</form>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
