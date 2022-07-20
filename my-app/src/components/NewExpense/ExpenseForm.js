50c27464b0bd9ae6fbc0553a35e2f5639152319a
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2022-07-20 17:16:31 -0400

    2 way binding
    - allows states to change while data is stored
    - we see state change here when we submit the form

--- my-app/src/components/NewExpense/ExpenseForm.js
+++ my-app/src/components/NewExpense/ExpenseForm.js
@@ -58,6 +58,10 @@ const ExpenseForm = () => {
         date: new Date(enteredDate)
     };
     console.log(expenseData);
+    //when the form is submited we go back to initial state
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -65,7 +69,9 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input type='text' 
+          value={enteredTitle} 
+          onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
@@ -73,6 +79,7 @@ const ExpenseForm = () => {
             type='number'
             min='0.01'
             step='0.01'
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -82,6 +89,7 @@ const ExpenseForm = () => {
             type='date'
             min='2019-01-01'
             max='2022-12-31'
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
