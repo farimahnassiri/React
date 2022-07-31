4c86290a2bd3a35d288cc7f2229a92a3b614e753
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2022-07-30 23:46:42 -0400

    equal sign

--- my-app/src/components/Expenses/Expenses.js
+++ my-app/src/components/Expenses/Expenses.js
@@ -15,7 +15,7 @@ const [filteredYear , setFilteredYear] = useState('2020');
 return(
     <div>
     <Card className="expenses">
-        <ExpensesFilter selected = {filteredYear} onChangeFilter = {filterChangeHandler} />
+        <ExpensesFilter selected={filteredYear} onChangeFilter = {filterChangeHandler} />
         <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
         <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
         <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
