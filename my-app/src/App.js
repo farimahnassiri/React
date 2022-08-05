d58a9d8c73255d940ef63d451b094edf6b23013b
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2022-08-05 00:41:15 -0400

    dynamically displaying list of new items

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,35 @@
-
+import React , { useState } from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    title:'Toilet Paper',
+    amount: 95,
+    date: new Date(2020, 7, 14)
+  },
+  {
+    title:'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28)
+  },
+  {
+    title:'New Desk (wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12)
+  }
+
+];
+
 // const App = () => {...}
 function App() {
-  const expenses = [
-    {
-      title:'Toilet Paper',
-      amount: 95,
-      date: new Date(2020, 7, 14)
-    },
-    {
-      title:'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28)
-    },
-    {
-      title:'New Desk (wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12)
-    }
-
-  ];
+  const [expenses , setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('In App JS');
-    console.log(expense);
-  }
+    setExpenses((prevExpenses) => {
+        return [expense, ...prevExpenses];
+    });
+  };
   return (
     <div>
       <NewExpense onAddExpense = {addExpenseHandler}/>
