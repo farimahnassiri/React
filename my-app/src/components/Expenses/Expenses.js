7fde20d42bab84e23425d0aa9a6267bea708db63
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2022-08-09 17:19:51 -0400

    using a constant to display value
    - using jsx snippet outside the return statement

--- my-app/src/components/Expenses/Expenses.js
+++ my-app/src/components/Expenses/Expenses.js
@@ -15,20 +15,25 @@ const [filteredYear , setFilteredYear] = useState('2020');
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     });    
-return (
-    <div>
-    <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeFilter = {filterChangeHandler} />
-        
-        {filteredExpenses.length === 0 && <p>No expenses found.</p>}
-        {filteredExpenses.length === 0 && filteredExpenses.map((expense) => (
+    //using JSX outside the return statement
+    let expenseContent = <p>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0){
+        expenseContent = filteredExpenses.map((expense) => (
             <ExpenseItem 
                 key = {expense.id}
                 title = {expense.title}
                 amount = {expense.amount}
                 date = {expense.date}
             />
-       ))}
+       ));
+    }
+
+return (
+    <div>
+    <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter = {filterChangeHandler} />
+        {expenseContent}
    
     </Card>
     </div>
