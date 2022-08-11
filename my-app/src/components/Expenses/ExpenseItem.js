67d1bed8189f6f52ef24e1d61d7b210905dc2df2
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2022-08-11 14:50:04 -0400

    updating some of the styling

--- my-app/src/components/Expenses/ExpenseItem.js
+++ my-app/src/components/Expenses/ExpenseItem.js
@@ -22,9 +22,8 @@ function ExpenseItem(props){
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>     
                 <h2>{title}</h2>
-                <div className='expense-item__price'>{props.amount}</div>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>change title</button>
         </Card>
     </li>
     );
