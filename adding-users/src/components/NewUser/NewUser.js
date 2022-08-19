3f19927225839d811554022b8e87fc064c117449
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2022-08-18 23:11:47 -0400

    new files created

---
+++ adding-users/src/components/NewUser/NewUser.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NewUser.css';
+
+const NewUser = (props) =>{
+
+    return
+    <form>
+        <input type='text'></input>
+        <input type=""></input>
+    </form>
+    ;
+};
+
+export default NewUser;
