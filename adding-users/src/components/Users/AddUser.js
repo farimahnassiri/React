ca21e58ab8961baffb5af43ea8e09069b74bc106
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2022-10-25 13:41:59 -0400

    card component added

--- adding-users/src/components/Users/AddUser.js
+++ adding-users/src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./AddUser.css";
+import Card from '../UI/Card';
 
 const AddUser = (props) => {
     const addUserHandler = (event) =>{
@@ -7,13 +8,16 @@ const AddUser = (props) => {
     };
 
     return (
-    <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text"/>
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"/>
-        <button type="submit">Add User</button>
-    </form>
+        <Card>
+             <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text"/>
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number"/>
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
+   
     );
 };
 
