86992eab3c8538984dc40515d3f718b2c1bba53e
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2022-11-06 16:09:00 -0500

    introducing refs
    using refs to read user input instead of states

--- adding-users-frag-port-ref/src/components/Users/AddUser.js
+++ adding-users-frag-port-ref/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,55 +6,65 @@ import ErrorModal from '../UI/ErrorModal';
 import classes from'./AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  
+  const nameInputRef = useRef(); 
+  const ageInputRef = useRef(); 
+
   const [error,setError] = useState();
 
-    const addUserHandler = (event) =>{
+    const addUserHandler = (event) => {
         event.preventDefault();
-        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+        if(enteredName.trim().length === 0 || enteredUserAge.trim().length === 0){
             setError({
                 title: 'Invalid input',
                 message: 'Please enter a valid name and age (non-empty values).'
             });
             return;
         }
-        if(+enteredAge < 1){
+        if(+enteredUserAge < 1){
             setError({
                 title: 'Invalid age',
                 message: 'Please enter a valid age (> 0).'
             });
             return;
         }
-        props.onAddUser(enteredUsername, enteredAge);
-        setEnteredUsername('');
-        setEnteredAge('');
+        props.onAddUser(enteredName, enteredUserAge);
+        //we are using refs instead of states so no need to reset them!
+        //we should also rarely use REFs to manipulate the DOM like this but here its fine
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
+
     };
 
-    const errorHandler = () =>{
+    const errorHandler = () => {
         setError(null);
     };
 
-    const usenameChangeHandler = (event) =>{
-        setEnteredUsername(event.target.value);
-    };
-
-    const ageChangeHandler = (event) =>{
-        setEnteredAge(event.target.value);
-    };
-
-    
-
     return (
         <Fragment>
-            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
-            <Card className={classes.input}>
-                <form onSubmit={addUserHandler}>
-                    <label htmlFor="username">Username</label>
-                    <input id="username" type="text" value={enteredUsername} onChange={usenameChangeHandler}/>
-                    <label htmlFor="age">Age (Years)</label>
-                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
-                    <Button type="submit">Add User</Button>
+            {error && <ErrorModal 
+            title = {error.title} 
+            message = {error.message} 
+            onConfirm = {errorHandler}/>}
+            <Card className = {classes.input}>
+                <form onSubmit = {addUserHandler}>
+                    <label htmlFor = "username">Username</label>
+                    <input 
+                    id = "username"
+                    type = "text"
+                    ref = {nameInputRef}
+                    />
+                    <label htmlFor = "age">Age (Years)</label>
+                    <input 
+                    id = "age"
+                    type = "number"
+                    ref = {ageInputRef}
+                    />
+                    <Button type = "submit">Add User</Button>
                 </form>
             </Card>
         </Fragment>
