9378a5c120154fe91f4bbe0d4bd2bc57a8ac2872
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-01-09 13:25:32 -0500

    adding more side effects

--- authentication-screen/src/components/Login/Login.js
+++ authentication-screen/src/components/Login/Login.js
@@ -12,6 +12,14 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(()=>{
+    console.log('EFFECT RUNNING');
+    
+    return () => {
+      console.log('CLEANUP EFFECT');
+    }
+  },[]);
+
+  useEffect(()=>{
     
     const identifier = setTimeout(() => {
       console.log('Cheking for validity!');
