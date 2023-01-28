ac217a161c0d2558afc7203de3f1421b8773afa2
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-01-28 16:27:48 -0500

    the meal component

--- project-food-order-app/src/components/Layout/HeaderCartButton.js
+++ project-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -3,7 +3,7 @@ import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 
-const HeaderCardButton = (props) => {
+const HeaderCardButton = () => {
 return <button className={classes.button}>
     <span className={classes.icon}>
         <CartIcon/>
