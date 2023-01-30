a40e8fc4927437f45cb51a1029221a6dc46378bc
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-01-29 23:30:59 -0500

    updating the overlay and cart buttons

--- project-food-order-app/src/components/Layout/HeaderCartButton.js
+++ project-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -3,8 +3,8 @@ import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 
-const HeaderCardButton = () => {
-return <button className={classes.button}>
+const HeaderCardButton = (props) => {
+return <button className={classes.button} onClick={props.onClicking}>
     <span className={classes.icon}>
         <CartIcon/>
     </span>
--- project-food-order-app/src/components/UI/Modal.js
+++ project-food-order-app/src/components/UI/Modal.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-    return <div className={classes.backdrop} />
+    return <div className={classes.backdrop} onClick={props.onClose}/>
 };
 
 const ModalOverlay = (props) => {
@@ -17,8 +17,10 @@ const portalElemnet = document.getElementById('overlays');
 
 const Modal = (props) => {
     return <Fragment>
-        {ReactDOM.createPortal(<Backdrop/>, portalElemnet)}
-        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElemnet)}
+        {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElemnet)}
+        {ReactDOM.createPortal(
+            <ModalOverlay>{props.children}</ModalOverlay>
+        , portalElemnet)}
     </Fragment>
 };
 
