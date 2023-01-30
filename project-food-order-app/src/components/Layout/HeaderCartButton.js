61d485477d03f6c0087467603b134c7f314a308d
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-01-30 12:16:14 -0500

    updating the cart numbers
    - using the .reduce() method in JS, to bundle multiples of the same food item as a single entry

--- project-food-order-app/src/components/Layout/HeaderCartButton.js
+++ project-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 
 const HeaderCardButton = (props) => {
+    const cartctx = useContext(CartContext);
+    //we can do .length for a bundle of items
+    const numberOfCartItems = cartctx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
 return <button className={classes.button} onClick={props.onClicking}>
     <span className={classes.icon}>
         <CartIcon/>
     </span>
     <span>Your Cart</span>
-    <span className={classes.badge}>3</span>
+    <span className={classes.badge}>{numberOfCartItems}</span>
 </button>
 };
 
