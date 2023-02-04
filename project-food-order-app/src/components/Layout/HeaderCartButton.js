271c219d9d9092889a66ba523a4dc62bed8a9330
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-02-04 14:58:49 -0500

    bumping card visual effect
    - adding items adds or removes the css visual effect

--- project-food-order-app/src/components/Layout/HeaderCartButton.js
+++ project-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,22 +1,46 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 
 const HeaderCardButton = (props) => {
-    const cartctx = useContext(CartContext);
+    const cartCtx = useContext(CartContext);
+    const [btnHighlighted, setBtnIsHighlighted] = useState(false);
+    
+    const { items } = cartCtx;
     //we can do .length for a bundle of items
-    const numberOfCartItems = cartctx.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
-return <button className={classes.button} onClick={props.onClicking}>
-    <span className={classes.icon}>
-        <CartIcon/>
-    </span>
-    <span>Your Cart</span>
-    <span className={classes.badge}>{numberOfCartItems}</span>
-</button>
+
+    const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0){
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+
+    }, [items]);
+
+    return (
+        <button className={btnClasses} onClick={props.onClicking}>
+            <span className={classes.icon}>
+                <CartIcon/>
+            </span>
+            <span>Your Cart</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
+        </button>
+    )
 };
 
 export default HeaderCardButton;
