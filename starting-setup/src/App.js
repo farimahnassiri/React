fd12c4db48483caf9332fe68a7d9622f838da8ff
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-02-05 02:32:28 -0500

    toggling button and changing states

--- starting-setup/src/App.js
+++ starting-setup/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((previousShowParagraph) => !previousShowParagraph);
+  };
+  
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
