4c179eff5bcc47f062839e66ca58532e80064161
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-02-09 18:05:11 -0500

    how useCallback works
    - must alway pass the dependencies array to the useCallback() func.

--- starting-setup/src/App.js
+++ starting-setup/src/App.js
@@ -6,16 +6,24 @@ import Button from './components/UI/Button/Button';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((previousShowParagraph) => !previousShowParagraph);
-  },[]);
+    if(allowToggle){
+      setShowParagraph((previousShowParagraph) => !previousShowParagraph);
+    }
+  },[allowToggle]);
   
+  const allowToggleHandler =() => {
+    setAllowToggle(true);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false}/>
+      <DemoOutput show={showParagraph}/>
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
