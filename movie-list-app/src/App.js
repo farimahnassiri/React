6a167165f4eab7294fa93df4a879eb7e3310f64d
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-02-18 22:47:56 -0500

    adding conditional states

--- movie-list-app/src/App.js
+++ movie-list-app/src/App.js
@@ -6,8 +6,10 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  async function fetchMoviesHandler() {  
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch('https://swapi.py4e.com/api/films');
     const data = await response.json();
     
@@ -20,6 +22,7 @@ function App() {
         };
       });
         setMovies(transformedMovies);
+        setIsLoading(false);
   };
   
 
@@ -30,7 +33,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length >0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
