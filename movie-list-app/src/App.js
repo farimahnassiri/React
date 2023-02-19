d5d75dbb1bef88435faf428b6e80c1d73f4ade55
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-02-18 22:39:21 -0500

    simplifying the code
    - alternative to .then's

--- movie-list-app/src/App.js
+++ movie-list-app/src/App.js
@@ -7,23 +7,21 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () => {  
-    fetch('https://swapi.py4e.com/api/films')
-      .then( (response) => {
-        return response.json();
-      })
-      .then( (data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date
-          };
+  async function fetchMoviesHandler() {  
+    const response = await fetch('https://swapi.py4e.com/api/films');
+    const data = await response.json();
+    
+    const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
       });
         setMovies(transformedMovies);
-      });
-  }
+  };
+  
 
 
   return (
