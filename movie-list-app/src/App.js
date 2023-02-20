380d67cd32eff9b59cf4409270bc9942413aa957
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-02-19 23:42:47 -0500

    fetching data initially without pressing btn
    - using useEffect and useCallback hooks to fetch data and send http request without pressing the button, when you first land on the page

--- movie-list-app/src/App.js
+++ movie-list-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 
 import MoviesList from './components/MoviesList';
@@ -9,8 +9,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -35,7 +34,12 @@ function App() {
         setError(error.message);
     }
     setIsLoading(false);
-  };
+  }, []);
+
+  useEffect(()=>{
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
 
     let content = <p>Found no movies.</p>;
     if(movies.length >0){
