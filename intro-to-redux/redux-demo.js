0f4bc83da1577ef98632f9220b609db799059d2d
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-03-10 16:41:56 -0500

    how redux works in react

--- intro-to-redux/redux-demo.js
+++ intro-to-redux/redux-demo.js
@@ -1,9 +1,17 @@
 const redux = require('redux');
 
-const counterReducer = (state = { counter: 0}, action) => {
-    return {
-        counter: state.counter + 1
-    };
+const counterReducer = (state = { counter: 0 }, action) => {
+    if(action.type === 'increment'){
+        return {
+            counter: state.counter + 1
+        };
+    }
+    if(action.type === 'decrement'){
+        return {
+            counter: state.counter - 1
+        };
+    }
+    return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -16,3 +24,4 @@ const counterSubscriber = () => {
 
 store.subscribe(counterSubscriber);
 store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });
