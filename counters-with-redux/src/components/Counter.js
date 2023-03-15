b14219c62112f4a587b819e1ee50a4e3b0eb279f
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-03-15 00:31:51 -0400

    login and logout buttons setup
    - using multiple slices to manage multiple states

--- counters-with-redux/src/components/Counter.js
+++ counters-with-redux/src/components/Counter.js
@@ -1,12 +1,12 @@
-import { useSelector, useDispatch, connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { counterActions } from '../store';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
-  const show = useSelector(state => state.showCounter);
+  const counter = useSelector(state => state.counter.counter);
+  const show = useSelector(state => state.counter.showCounter);
   
   const incrementHandler = () => {
     dispatch(counterActions.increment());
