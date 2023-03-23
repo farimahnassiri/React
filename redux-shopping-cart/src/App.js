5d674bc2b8ee783310a2a2ba6976b23b577d1d21
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-03-23 00:08:16 -0400

    updating the cart and sending request to backend

--- redux-shopping-cart/src/App.js
+++ redux-shopping-cart/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -5,7 +6,16 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  
+  useEffect(()=>{
+    fetch('https://react-http-15f97-default-rtdb.firebaseio.com/cart.json',{
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    })
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
