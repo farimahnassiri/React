355c418014bfe1e622b5a8b4edfe8988eec2a5c8
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-03-27 23:26:32 -0400

    returning a function with our action creator

--- redux-shopping-cart/src/store/cart-slice.js
+++ redux-shopping-cart/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -40,6 +41,54 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://react-http-15f97-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+        }
+      );
+  
+      if (!response.ok){
+        throw new Error('Sending cart data failed.');
+      }
+    };
+    //nesting is require if we want to try and catch errors because of the nature of fetch
+    try{
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data Successfully!'
+        })
+      );
+    } catch(error){
+      sendCartData().catch((error) => {
+        dispatch(
+          uiActions.showNotification({
+            status: 'error',
+            title: 'Error!',
+            message: 'Sending cart data failed!'
+         })
+        ); 
+      });
+    }
+
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
