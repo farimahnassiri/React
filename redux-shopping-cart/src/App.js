716f59ba309d5c97604c6d6f63e2459a7efdd3f9
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-03-27 23:02:55 -0400

    code cleanup

--- redux-shopping-cart/src/App.js
+++ redux-shopping-cart/src/App.js
@@ -14,41 +14,6 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
-  
-  // useEffect(() => {
-  //   const sendCartData = async () => {
-  //     dispatch(uiActions.showNotification({
-  //         status: 'pending',
-  //         title: 'Sending...',
-  //         message: 'Sending cart data!'
-  //       })
-  //     );
-  //     const response = await fetch('https://react-http-15f97-default-rtdb.firebaseio.com/cart.json',{
-  //         method: 'PUT',
-  //         body: JSON.stringify(cart)
-  //       });
-
-  //     if (!response.ok){
-  //       throw new Error('Sending cart data failed.');
-  //     }
-
-  //     dispatch(uiActions.showNotification({
-  //         status: 'success',
-  //         title: 'Success!',
-  //         message: 'Sent cart data Successfully!'
-  //       })
-  //     );
-  //   };
-  //     sendCartData().catch((error) => {
-  //       dispatch(uiActions.showNotification({
-  //           status: 'error',
-  //           title: 'Error!',
-  //           message: 'Sending cart data failed!'
-  //        })
-  //       ); 
-  //     });
-  
-  // }, [cart, dispatch]);
 
   useEffect(() => {
     const sendCartData = async () => {
