846bbda5c59c1f991797024502147015d9b8b09d
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-03-31 15:29:45 -0400

    updating the navigation bar

--- mpa-with-router/src/App.js
+++ mpa-with-router/src/App.js
@@ -7,6 +7,7 @@ from "react-router-dom";
 
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
 
 // const routerDefinitions = createRoutesFromElements(
 //   <Route>
@@ -17,8 +18,11 @@ import ProductsPage from './pages/Products';
 
 // the path is the part after the domain
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage/> },
-  { path: '/products', element: <ProductsPage/> }
+  { path: '/', element: <RootLayout/>, children: [
+    { path: '/', element: <HomePage/> },
+    { path: '/products', element: <ProductsPage/> }
+  ]},
+
 ]);
 
 //const router = createBrowserRouter(routerDefinitions);
