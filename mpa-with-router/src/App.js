07477f1133d9e1356fbc353856843acea3c63d11
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-03-31 01:08:33 -0400

    routing through pages with Links

--- mpa-with-router/src/App.js
+++ mpa-with-router/src/App.js
@@ -8,20 +8,20 @@ from "react-router-dom";
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 
-const routerDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage/>} />
-    <Route path="/products" element={<ProductsPage/>} />
-  </Route>
-);
+// const routerDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage/>} />
+//     <Route path="/products" element={<ProductsPage/>} />
+//   </Route>
+// );
 
 // the path is the part after the domain
-// const router = createBrowserRouter([
-//   { path: '/', element: <HomePage/> },
-//   { path: '/products', element: <ProductsPage/> }
-// ]);
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage/> },
+  { path: '/products', element: <ProductsPage/> }
+]);
 
-const router = createBrowserRouter(routerDefinitions);
+//const router = createBrowserRouter(routerDefinitions);
 
 function App() {
   return <RouterProvider router={router}/>;
