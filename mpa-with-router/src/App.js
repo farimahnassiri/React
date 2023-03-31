89bd58f748272e3ac718a5c7c6babc26b57e5bcb
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-03-31 01:00:14 -0400

    routing with JSX
    - alternative way of routing

--- mpa-with-router/src/App.js
+++ mpa-with-router/src/App.js
@@ -1,14 +1,27 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { 
+  createBrowserRouter,
+  createRoutesFromElements, 
+  RouterProvider, 
+  Route } 
+from "react-router-dom";
 
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 
+const routerDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage/>} />
+    <Route path="/products" element={<ProductsPage/>} />
+  </Route>
+);
 
 // the path is the part after the domain
-const router = createBrowserRouter([
-  { path: '/', element: <HomePage/> },
-  { path: '/products', element: <ProductsPage/> }
-]);
+// const router = createBrowserRouter([
+//   { path: '/', element: <HomePage/> },
+//   { path: '/products', element: <ProductsPage/> }
+// ]);
+
+const router = createBrowserRouter(routerDefinitions);
 
 function App() {
   return <RouterProvider router={router}/>;
