d83e829eb45bfbb0b10d05fd2a0406f3b8fae877
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-03-31 00:47:09 -0400

    using the router for the home page

--- mpa-with-router/src/App.js
+++ mpa-with-router/src/App.js
@@ -1,5 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+
+
+// the path is the part after the domain
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage/>},
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
