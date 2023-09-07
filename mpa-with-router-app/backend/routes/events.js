7044771af4cd46085e1b4dd87000cbf06e39a73a
farimahnassiri <farimah.nassiri@gmail.com>
2023-09-07 14:50:21 -0400

    removing the delay

--- mpa-with-router-app/backend/routes/events.js
+++ mpa-with-router-app/backend/routes/events.js
@@ -13,10 +13,10 @@ router.get('/', async (req, res, next) => {
   try {
     const events = await getAll();
     //adding timeout to make sure data is loaded before user navigates to the page
-    setTimeout(() => {
-      res.json({ events: events });
-    }, 1500);
-    //res.json({ events: events });
+    // setTimeout(() => {
+    //   res.json({ events: events });
+    // }, 1500);
+    res.json({ events: events });
   } catch (error) {
     next(error);
   }
