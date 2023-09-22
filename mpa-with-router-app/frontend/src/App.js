4fade1675da0499b5194542436b593510c7218e1
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-09-22 19:36:48 -0400

    adding newsletter component to main navigation

--- mpa-with-router-app/frontend/src/App.js
+++ mpa-with-router-app/frontend/src/App.js
@@ -1,22 +1,19 @@
-import { 
-  createBrowserRouter,
-  createRoutesFromElements, 
-  RouterProvider, 
-  Route } 
-from "react-router-dom";
 
-import HomePage from './pages/Home';
-import ErrorPage from './pages/Error';
-import EventsPage, { loader as eventsLoader} from './pages/Events';
-import EventDetailsPage, {
-  loader as eventDetailLoader,
-  action as deleteEventAction
-} from './pages/EventDetail';
-import NewEventPage from './pages/NewEvent';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import EditEventPage from './pages/EditEvent';
-import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from './pages/EventDetail';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventsRootLayout from './pages/EventsRoot';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 
 // Challenge / Exercise
@@ -42,51 +39,58 @@ import { action as manipulateEventAction } from './components/EventForm';
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 // the path is the part after the domain
+
 const router = createBrowserRouter([
   {
-      path: '/',
-      element: <RootLayout/>,
-      errorElement: <ErrorPage/>,
-      children: [
-        { index: true, element: <HomePage/>},
-        { path: 'events', element: <EventsRootLayout />,
-          children:[
-            { 
-              index: true,
-              element: <EventsPage />,
-              loader: eventsLoader
-            },
-            { 
-              path: ':eventId',
-              id: 'event-detail',
-              loader: eventDetailLoader,
-              children:[
-                { 
-                  index: true,
-                  element: <EventDetailsPage />,
-                  action: deleteEventAction
-                },
-                { 
-                  path: 'edit',
-                  element: <EditEventPage />,
-                  action: manipulateEventAction
-                },
-              ],
-            },
-            {
-              path: 'new',
-              element: <NewEventPage />,
-              action: manipulateEventAction
-            },
-          ]
-       },
-    ]
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: 'events',
+        element: <EventsRootLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: ':eventId',
+            id: 'event-detail',
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
+          },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+        ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
+    ],
   },
 ]);
 
-
 function App() {
-  return <RouterProvider router={router}/>;;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
