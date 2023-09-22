089eaa4f48298ffc32080374f035f6231c6db28e
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-09-22 13:56:44 -0400

    useNavigation() hook

--- mpa-with-router-app/frontend/src/components/EventForm.js
+++ mpa-with-router-app/frontend/src/components/EventForm.js
@@ -1,8 +1,9 @@
-import { useNavigate, Form, useNavigation } from 'react-router-dom';
+import { useNavigate, Form, useNavigation, useActionData } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
+  const data = useActionData();
   const navigate = useNavigate();
   const navigation = useNavigation();
 
@@ -13,6 +14,11 @@ function EventForm({ method, event }) {
 
   return (
     <Form method='post' className={classes.form}>
+      {data && data.errors && <ul>
+        {Object.values(data.errors).map((err) =>(
+          <li key={err}>err</li>
+        ))}
+        </ul>}
       <p>
         <label htmlFor="title">Title</label>
         <input
