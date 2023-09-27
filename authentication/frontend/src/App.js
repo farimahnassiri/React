ae3cb14cffdabea528bf61325c7fab43fbce4233
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-09-27 12:53:06 -0700

    adding checkAuthLoader()
    - to make sure user's are not able to reach the /events/new page to create new events if they have not logged in

--- authentication/frontend/src/App.js
+++ authentication/frontend/src/App.js
@@ -15,7 +15,8 @@ import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, {action as authAction} from './pages/Authentication';
 import { action as logoutAction } from './pages/Logout';
-import { tokenLoader as tokenLoader } from './util/auth';
+import { tokenLoader, checkAuthLoader } from './util/auth';
+
 
 const router = createBrowserRouter([
   {
@@ -49,6 +50,7 @@ const router = createBrowserRouter([
                 path: 'edit',
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader: checkAuthLoader
               },
             ],
           },
@@ -56,6 +58,7 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthLoader
           },
         ],
       },
