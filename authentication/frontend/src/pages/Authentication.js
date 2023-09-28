3f2219043247c45dae91a75f628c903f029ee8f0
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-09-28 11:19:53 -0700

    authentication completed
    - a better solution vs. the useEffect
    - keeping track of when the token was generated and how long the user has before signing out

--- authentication/frontend/src/pages/Authentication.js
+++ authentication/frontend/src/pages/Authentication.js
@@ -41,6 +41,9 @@ export async function action({request}) {
  const token = resData.token;
 
  localStorage.setItem('token', token);
+ const expiration = new Date();
+ expiration.setHours(expiration.getHours() + 1);
+ localStorage.setItem('expiration', expiration.toISOString());
 
  // soon: manage the Token
  return redirect('/');
