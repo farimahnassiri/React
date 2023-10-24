5cffe2b86a8fcc6201ec2e6b1f5846c25000613d
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-10-23 20:50:41 -0400

    Adding Links

--- nextjs-course/pages/news/index.js
+++ nextjs-course/pages/news/index.js
@@ -1,5 +1,20 @@
+import Link from 'next/link'
+import { Fragment } from 'react';
+
 function NewsPage(){
-    return <h1>The News Page</h1>;
-  }
+  return(
+    <Fragment>
+          <h1>The News Page</h1>
+          <ul>
+            <li>
+              <Link href="/news/nextjs-is-a-great-framework">
+                NextJS Is A Great Framework
+              </Link>
+            </li>
+            <li>Something Else</li>
+          </ul>
+    </Fragment>
+  );
+}
   
-  export default NewsPage;
+export default NewsPage;
