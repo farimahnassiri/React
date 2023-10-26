a405ead16d14865d853e2820d2d400dc8a70dfe3
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-10-25 22:34:43 -0400

    sending data to a server
    - using useEffect
    - need optimization because of re-rendering issue / 1st cycle

--- nextjs-meetup-app/pages/index.js
+++ nextjs-meetup-app/pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -18,7 +19,14 @@ const DUMMY_MEETUPS = [
 ];
 
 function HomePage(){
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+  
+  useEffect(() => {
+    // send a http request and fetch data
+    setLoadedMeetups(DUMMY_MEETUPS);
+  },[]);
+
+  return <MeetupList meetups={loadedMeetups} />;
 }
 
 export default HomePage;
