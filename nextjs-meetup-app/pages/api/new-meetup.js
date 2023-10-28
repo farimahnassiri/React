80be8a516952c609828a83a3537c169bc08e089e
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-10-28 18:16:23 -0400

    spacing

--- nextjs-meetup-app/pages/api/new-meetup.js
+++ nextjs-meetup-app/pages/api/new-meetup.js
@@ -7,13 +7,13 @@ async function handler (req, res){
 
         const client = await MongoClient.connect('mongodb+srv://farimahnassiri:BeMgCaSrBaRa@cluster0.xwlakya.mongodb.net/meetups?retryWrites=true&w=majority');
         const db = client.db();
-
         const meetupsCollection = db.collection('meetups');
+        
         const result = await meetupsCollection.insertOne(data);
 
         console.log(result);
         client.close();
-        
+
         res.status(201).json({ message: 'Meetup inserted!' });
     }
 }
