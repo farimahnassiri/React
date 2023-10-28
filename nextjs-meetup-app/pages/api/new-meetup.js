b77bc27a3da171a245484a8aff34b751c17db673
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-10-28 17:19:31 -0400

    connect with DB on MongoDB

--- nextjs-meetup-app/pages/api/new-meetup.js
+++ nextjs-meetup-app/pages/api/new-meetup.js
@@ -1,8 +1,20 @@
-function handler (req, res){
+import { MongoClient } from 'mongodb';
+
+async function handler (req, res){
     if (req.method == 'POST'){
         const data = req.body;
+       // const { title, image, address, description } = data;
 
-        const { title, image, address, description } = data;
+        const client = await MongoClient.connect('mongodb+srv://farimahnassiri:BeMgCaSrBaRa@cluster0.xwlakya.mongodb.net/meetups?retryWrites=true&w=majority');
+        const db = client.db();
+
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne(data);
+
+        console.log(result);
+        client.close();
+        
+        res.status(201).json({ message: 'Meetup inserted!' });
     }
 }
 
