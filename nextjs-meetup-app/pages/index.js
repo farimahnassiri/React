24362c75f71522c43848a52a574a2fad1a05404d
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-10-29 00:19:53 -0400

    fetching data from mongoDB

--- nextjs-meetup-app/pages/index.js
+++ nextjs-meetup-app/pages/index.js
@@ -1,21 +1,5 @@
+import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://www.contiki.com/media/vqlnvuon/oktoberfest-hotel-5-days-trip-14.jpeg?center=0.48756218905472637%2C0.5522388059701493&format=webp&mode=crop&width=2400&height=2400&quality=80',
-    address: 'Some address here!',
-    description: 'This is the first meetup!'
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image: 'https://media.npr.org/assets/img/2021/11/04/gettyimages-1236319784-917b8dfda5e2e0a5a9f7ae9946cd105ec4cabfe0-s1600-c85.webp',
-    address: 'Some address here!',
-    description: 'This is the second meetup!'
-  }
-];
 
 function HomePage(props){
   return <MeetupList meetups={props.meetups} />;
@@ -35,11 +19,26 @@ function HomePage(props){
 
 export async function getStaticProps(){
   // fetch data from an API
+  const client = await MongoClient.connect(
+    'mongodb+srv://farimahnassiri:BeMgCaSrBaRa@cluster0.xwlakya.mongodb.net/meetups?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+  
   return {
     props: {
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      }))
     },
-    revalidate: 10
+    revalidate: 1
   };
 }
 
