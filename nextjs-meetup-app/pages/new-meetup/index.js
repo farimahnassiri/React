f045317fd92d45565fc3c836ad0a55ea88ad9c86
farimahnassiri <46692395+farimahnassiri@users.noreply.github.com>
2023-10-29 10:07:28 -0400

    SEO optimization
    - adding meta tags using Head element

--- nextjs-meetup-app/pages/new-meetup/index.js
+++ nextjs-meetup-app/pages/new-meetup/index.js
@@ -1,4 +1,6 @@
+import { Fragment } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage(){
@@ -19,7 +21,18 @@ function NewMeetupPage(){
 
     router.push('/');
   }
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name='description'
+          content='Add your own meetups and create amazing networking opportunities.'
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
 }
   
 export default NewMeetupPage;
